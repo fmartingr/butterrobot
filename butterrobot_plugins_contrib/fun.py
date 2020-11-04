92f4696a15bd876cbe2a5f9b3d9bad94a1997032
Felipe M <me@fmartingr.com>
2020-11-04 13:25:31 +0100

    contrib.fun.coin

--- butterrobot_plugins_contrib/fun.py
+++ butterrobot_plugins_contrib/fun.py
@@ -1,3 +1,5 @@
+import random
+
 import dice
 
 from butterrobot.plugins import Plugin
@@ -21,3 +23,12 @@ class DicePlugin(Plugin):
         if message.text.startswith("!dice"):
             roll = int(dice.roll(message.text.replace("!dice ", "")))
             yield Message(chat=message.chat, reply_to=message.id, text=roll)
+
+
+class CoinPlugin(Plugin):
+    id = "contrib/fun/coin"
+
+    @classmethod
+    def on_message(cls, message: Message):
+        if message.text.startswith("!coin"):
+            yield Message(chat=message.chat, reply_to=message.id, text=random.choice(("heads", "tails")))
